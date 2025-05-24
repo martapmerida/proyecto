d620d59c588c37a84bb627e86fbedae19174d533
Marta <martapmerida@gmail.com>
2025-05-24 19:59:22 +0200

    Fix endpoint app2

--- app2/src/main/java/com/example/proyecto/ProyectoController.java
+++ app2/src/main/java/com/example/proyecto/ProyectoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProyectoController {
 
-	@GetMapping("/")
+	@GetMapping("/hello-world")
 	public String index() {
 		return "Greetings from Spring Boot! Hello, World!";
 	}
